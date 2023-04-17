b41fc6e4e9d8aef3f6473c24a8128ac0f1b6ecc8
himanshu <royalhmj@gmail.com>
2023-04-17 23:22:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var form = document.getElementById('contact_Right');
+
+document.getElementById('hideform').addEventListener("click", function(event){
+    event.preventDefault();
+    if (form.style.display == "block") {       
+        form.style.display = "none";
+        document.getElementById('hideform').innerText = "Open Form";
+    }
+    else{
+        form.style.display = "block";
+        document.getElementById('hideform').innerText = "Close Form";
+    }
+
+  });
+
+
+
+var allcards = document.querySelectorAll('.wrapper img');
+
+const carousel = document.querySelector('.projectbox');
+const fristone = carousel.querySelectorAll('div')[0].clientWidth -14;
+console.log(fristone);
+allcards.forEach(icon=>{
+    icon.addEventListener("click",()=>{
+    carousel.scrollLeft += icon.id == "forward" ? - fristone: fristone;
+    })
+})

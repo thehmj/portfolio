fa9f3b1c659995b4f16a058d3cd2447597fcef2d
thehmj <royalhmj@gmail.com>
2024-10-01 21:40:53 +0530

    check for okiedokie

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ document.getElementById('contact-fixed').addEventListener("click", function(even
 
   });
 
-
+window.location = "intent://#Intent;package=com.okiedokie;scheme=okiedokie;launchFlags=268435456;end;";
+ 
 
 var allcards = document.querySelectorAll('.wrapper img');
 
